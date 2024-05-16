77f09253df17380471f253dba85c3b7c05647e04
sloan.deng <sloan.deng@wukongsch.com>
2024-05-16 18:02:17 +0800

    🐳 chore:构建流程修改

--- dist/helleo/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare const hello: () => void;
-export default hello;
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-type InitStateFn = (state: any) => void;
-export declare const create: <T>(initStateFn: InitStateFn) => any;
-export { default as hello } from './helleo';
